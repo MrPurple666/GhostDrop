7768afbb6a4837e9360222aa8f2b53d25817bf6d
Antonio Sacramento <antoniosacramento666usa@gmail.com>
2026-09-07 17:20:31 -0300

    feat: delete private S3 objects

--- backend/src/main/java/dev/ghostdrop/infrastructure/s3/S3StorageService.java
+++ backend/src/main/java/dev/ghostdrop/infrastructure/s3/S3StorageService.java
@@ -2,6 +2,8 @@ package dev.ghostdrop.infrastructure.s3;
 
 import dev.ghostdrop.domain.StorageService;
 import java.time.Duration;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -11,11 +13,17 @@ import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignReques
 
 public final class S3StorageService implements StorageService {
     private final String bucket;
+    private final S3Client client;
     private final S3Presigner presigner;
     private final Duration uploadLifetime;
 
     public S3StorageService(String bucket, S3Presigner presigner, Duration uploadLifetime) {
+        this(bucket, null, presigner, uploadLifetime);
+    }
+
+    public S3StorageService(String bucket, S3Client client, S3Presigner presigner, Duration uploadLifetime) {
         this.bucket = bucket;
+        this.client = client;
         this.presigner = presigner;
         this.uploadLifetime = uploadLifetime;
     }
@@ -31,5 +39,10 @@ public final class S3StorageService implements StorageService {
 
     public String createDownloadUrl(String storageKey, Duration lifetime) {
         return presigner.presignGetObject(GetObjectPresignRequest.builder().signatureDuration(lifetime).getObjectRequest(GetObjectRequest.builder().bucket(bucket).key(storageKey).build()).build()).url().toString();
+    }
+
+    public void delete(String storageKey) {
+        if (client == null) throw new IllegalStateException("S3 client is required for deletion");
+        client.deleteObject(DeleteObjectRequest.builder().bucket(bucket).key(storageKey).build());
     }
 }

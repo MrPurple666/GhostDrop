0c76a9e284b11d25801bf00999b1a1a91f341b89
Antonio Sacramento <antoniosacramento666usa@gmail.com>
2026-09-07 17:09:51 -0300

    feat: generate direct S3 upload URLs

---
+++ backend/src/main/java/dev/ghostdrop/infrastructure/s3/S3StorageService.java
@@ -0,0 +1,30 @@
+package dev.ghostdrop.infrastructure.s3;
+
+import dev.ghostdrop.domain.StorageService;
+import java.time.Duration;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+public final class S3StorageService implements StorageService {
+    private final String bucket;
+    private final S3Presigner presigner;
+    private final Duration uploadLifetime;
+
+    public S3StorageService(String bucket, S3Presigner presigner, Duration uploadLifetime) {
+        this.bucket = bucket;
+        this.presigner = presigner;
+        this.uploadLifetime = uploadLifetime;
+    }
+
+    @Override
+    public String createUploadUrl(String storageKey, String contentType, long fileSize) {
+        PresignedPutObjectRequest request = presigner.presignPutObject(PutObjectPresignRequest.builder()
+                .signatureDuration(uploadLifetime)
+                .putObjectRequest(PutObjectRequest.builder().bucket(bucket).key(storageKey).contentType(contentType).contentLength(fileSize).build())
+                .build());
+        return request.url().toString();
+    }
+}
